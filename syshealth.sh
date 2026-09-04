7cbcbcf8b352aa4f4d06a55521b303cf907ffc46
Student <student@localhost.localdomain>
2026-09-04 11:06:43 -0400

    Added shebang and made syshealth.sh executable

---
+++ syshealth.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# ==================================================
+# syshealth.sh - System Health & Log Analysis Toolkit
+# Lab 1 - Data Collector
+# Author: Aidan Zhang
+# Date : 9/4/2026
+#bash: line 1: Y: command not found
+# ==================================================
+
